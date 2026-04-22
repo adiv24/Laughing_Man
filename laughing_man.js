760a6a09288f5e0b37008e936305ed66239e414f
Adiv <adiv.sethi1@gmail.com>
2026-04-22 10:39:36 -0400

    Starting movement

--- laughing_man.js
+++ laughing_man.js
@@ -13,9 +13,17 @@ for(let i = 0; i < SQUARE_COUNT; i++) {
     square.alt = "Catch the Laughing Man!";
     square.className = 'square';
     box.appendChild(square);
-
-
-
-
-
 }
+Array.from(box.children).forEach((element)=>{
+          let dx = SPEED * Math.random()*2-1;
+            let dy = SPEED * Math.random()*2-1;
+            
+            let x = parseInt(element.style.left) || 0;
+            let y = parseInt(element.style.top) || 0;
+            setInterval(()=>{
+                x += dx;
+                y += dy;
+                element.style.left = x + 'px';
+                element.style.top = y + 'px';
+            }, TIMER_SPEED)
+    });
