77a118e4ce2bc95b56324b325fc7ea5d0ce58880
adiv24 <adiv.sethi1@gmail.com>
2026-05-09 14:01:12 -0400

    Added square count and speed variables

--- laughing_man.js
+++ laughing_man.js
@@ -1,4 +1,6 @@
 const SQUARE_COUNT = 3;
+const SPEED = 2;
+const TIMER_SPEED = 10;
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#square').addEventListener('click', () => {
@@ -7,23 +9,25 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 let box = document.querySelector('#box');
-for(let i = 0; i < SQUARE_COUNT; i++) {
+
+for (let i = 0; i < SQUARE_COUNT; i++) {
     let square = document.createElement('div');
     square.src = 'laughing_man.jpg';
     square.alt = "Catch the Laughing Man!";
     square.className = 'square';
     box.appendChild(square);
 }
-Array.from(box.children).forEach((element)=>{
-          let dx = SPEED * Math.random()*2-1;
-            let dy = SPEED * Math.random()*2-1;
-            
-            let x = parseInt(element.style.left) || 0;
-            let y = parseInt(element.style.top) || 0;
-            setInterval(()=>{
-                x += dx;
-                y += dy;
-                element.style.left = x + 'px';
-                element.style.top = y + 'px';
-            }, TIMER_SPEED)
-    });
+
+Array.from(box.children).forEach((element) => {
+    let dx = SPEED * (Math.random() * 2 - 1);
+    let dy = SPEED * (Math.random() * 2 - 1);
+    let x = parseInt(element.style.left) || 0;
+    let y = parseInt(element.style.top) || 0;
+
+    setInterval(() => {
+        x += dx;
+        y += dy;
+        element.style.left = x + 'px';
+        element.style.top = y + 'px';
+    }, TIMER_SPEED);
+});
